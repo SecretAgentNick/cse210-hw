53613c83edb5fee24d6af96d44aca58d7df84e45
SecretAgentNick <coolascows@hotmail.com>
2024-01-18 15:29:43 -0700

    Resume/Job Prog FInished

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "The New Guy";
+        job1._company = "The New Store";
+        job1._startYear = 2091;
+        job1._endYear = 2061;
+
+        Job job2 = new Job();
+        job2._jobTitle = "The Old Guy";
+        job2._company = "The Old Store";
+        job2._startYear = 647;
+        job2._endYear = 9999;
+
+        Resume userResume = new Resume();
+        userResume._name = "Nicholas Bastian";
+        userResume._jobs.Add(job1);
+        userResume._jobs.Add(job2);
+        userResume.Display();
     }
 }
