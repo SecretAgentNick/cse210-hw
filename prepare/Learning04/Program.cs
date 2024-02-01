26c9f9e3303e7f709a4f880b859591eee2bd34e0
SecretAgentNick <coolascows@hotmail.com>
2024-01-31 20:07:33 -0700

    completed learn4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Josh Guy", "Addition");
+        System.Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Not Josh", "Subtractions", "7.3", "8-19");
+        System.Console.WriteLine(a2.GetSummary());
+        System.Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("This is Josh", "Comic Books", "Who isn't batman");
+        System.Console.WriteLine(a3.GetSummary());
+        System.Console.WriteLine(a3.GetWritingInformation());
     }
 }
